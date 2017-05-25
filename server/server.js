5144e47e46fc6f896dcbfa585f45a681e6da523d
Peter Jahelka <prjahelka@msn.com>
2017-05-25 12:37:47 -0600

    chat messages

--- server/server.js
+++ server/server.js
@@ -15,6 +15,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('user connected');
 
+    socket.emit('newEmail', {
+        from: 'pete@soomedomain.com',
+        text: 'How\'s it going?'
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('new email: ', newEmail);
+    });
+
+    socket.on('createMessage', (chat) => {
+        console.log('chat message: ', chat);
+    });
+
+    socket.emit('newMessage', {
+        from: 'pete@soomedomain.com',
+        text: 'How\'s it going?',
+        createdAt: 123
+    });
+
     socket.on('disconnect', () => {
         console.log('user connection lost');
     });
